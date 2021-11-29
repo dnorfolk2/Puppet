58325e831297b527236be10a86316c5620c963dc
dnorfolk2 <d.norfolk2627@student.leedsbeckett.ac.uk>
2021-11-29 21:36:25 +0000

    Cleaned up configpp

--- OpenSMTPD_661_RCE/manifests/config.pp
+++ OpenSMTPD_661_RCE/manifests/config.pp
@@ -6,19 +6,6 @@ class opensmtpd_661::config {
     $strings_to_leak = $secgen_parameters['/opt/pachev_ftp/pachev_ftp-master/ftp_server/target/release/conf/users.cfg maybe?']
     $strings_to_pre_leak = $secgen_parameters['/opt/pachev_ftp/pachev_ftp-master/ftp_server/target/release/ftproot/ftpusr/hint_file.txt']
 
-    # if $raw_org and $raw_org[0] and $raw_org[0] != '' {
-    #     $organisation = parsejson($raw_org[0])
-    # } else {
-    #     $organisation = ''
-    # }
-    # file { '/etc/proftpd/proftpd.conf':
-    #     ensure   => present,
-    #     owner    => 'root',
-    #     group    => 'root',
-    #     mode     => '0644',
-    #     content  => template('proftpd_133c_backdoor/proftpd.erb')
-    # }
-
     # ::secgen_functions::leak_files { 'proftpd_133c_backdoor-file-leak':
     #     storage_directory => '/root',
     #     leaked_filenames  => $leaked_filenames,
