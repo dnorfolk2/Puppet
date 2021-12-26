d6b93226f2557df797df3503cb2d5a081d48cf06
dnorfolk2 <d.norfolk2627@student.leedsbeckett.ac.uk>
2021-12-26 09:07:32 +0000

    updated freeftpd install code

--- freeftpd/manifests/install.pp
+++ freeftpd/manifests/install.pp
@@ -9,8 +9,4 @@ class freeftpd::install {
      source => 'C:\Users\vagrant\Downloads\freeftpd.exe',
      install_options => ['/sAll']
    }
-
-   exec { 'set_as_services':
-    command => '"C:\Program Files (x86)\freeFTPd\FreeFTPDService.exe" /service',
-  }
 }
